981024cf364dd55142e91a6cd1fbb8c91e7b156d
Ricardo Delfin <me@rdelfin.com>
2023-12-02 23:18:19 +0000

    Refactoring of aoc1

--- src/bin/aoc1.rs
+++ src/bin/aoc1.rs
@@ -1,7 +1,11 @@
-use aoc_2023::Opt;
+use aoc_2023::{trie::Trie, Opt};
 use clap::Parser;
 use color_eyre::eyre::Result;
-use std::{collections::HashMap, fs::File};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader, Read},
+};
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -60,13 +64,95 @@ fn main() -> Result<()> {
 
     let numbers = if let Some(file_name) = opt.file_name {
         let file = File::open(file_name)?;
-        aoc_2023::get_line_numbers(file, &digits, &reverse_digits)?
+        get_line_numbers(file, &digits, &reverse_digits)?
     } else {
-        aoc_2023::get_line_numbers(std::io::stdin(), &digits, &reverse_digits)?
+        get_line_numbers(std::io::stdin(), &digits, &reverse_digits)?
     };
 
     let sum: u32 = numbers.into_iter().sum();
     println!("{sum}");
 
     Ok(())
+}
+
+// This function takes a line and returns, respectively, the first and last digit, if they exist
+fn get_first_digit<I: Iterator<Item = char> + Clone>(
+    line_iter: I,
+    digit_map: &HashMap<&'static str, u32>,
+    trie: &Trie,
+) -> Option<u32> {
+    let mut result = None;
+    let mut searcher = trie.get_searcher();
+
+    let mut char_iter = line_iter.clone();
+    let mut last_word_iter = line_iter.clone();
+    while let Some(c) = char_iter.next() {
+        match searcher.advance(c) {
+            Ok(Some(val)) => {
+                for _ in 0..searcher.len() {
+                    last_word_iter.next().expect("should match char_iter");
+                }
+                let digit = digit_map
+                    .get(&val[..])
+                    .expect("must exist due to construction");
+                result = Some(*digit);
+                break;
+            }
+            Err(_) => {
+                // If we get no matches, we need to go all the way back to the first character we
+                // processed and skip just that one
+                last_word_iter
+                    .next()
+                    .expect("next should exist as we've seen a word before");
+                char_iter = last_word_iter.clone();
+                searcher = trie.get_searcher();
+            }
+            _ => {}
+        }
+    }
+
+    result
+}
+
+// Returns the first and last digit all the lines provided
+pub fn get_line_numbers<R: Read>(
+    r: R,
+    fwd_digit_map: &HashMap<&'static str, u32>,
+    bwd_digit_map: &HashMap<&'static str, u32>,
+) -> std::io::Result<Vec<u32>> {
+    let fwd_trie = get_trie(fwd_digit_map);
+    let bwd_trie = get_trie(bwd_digit_map);
+
+    let mut reader = BufReader::new(r);
+    let mut numbers = Vec::new();
+
+    loop {
+        let mut line = String::new();
+        if reader.read_line(&mut line)? == 0 {
+            break;
+        }
+
+        let first_digit = if let Some(d) = get_first_digit(line.chars(), &fwd_digit_map, &fwd_trie)
+        {
+            d
+        } else {
+            println!("line has no digits");
+            continue;
+        };
+        let last_digit =
+            if let Some(d) = get_first_digit(line.chars().rev(), &bwd_digit_map, &bwd_trie) {
+                d
+            } else {
+                println!("line has no digits");
+                continue;
+            };
+
+        numbers.push(first_digit * 10 + last_digit);
+    }
+
+    Ok(numbers)
+}
+
+fn get_trie(digit_map: &HashMap<&'static str, u32>) -> Trie {
+    Trie::new(digit_map.iter().map(|(s, _)| *s))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,96 +1,36 @@
-use crate::trie::Trie;
 use clap::Parser;
 use std::{
-    collections::HashMap,
+    fs::File,
     io::{BufRead, BufReader, Read},
+    path::PathBuf,
 };
 
-mod trie;
+pub mod block_game;
+pub mod trie;
 
 #[derive(Parser, Debug)]
 pub struct Opt {
     #[clap(index = 1)]
-    pub file_name: Option<String>,
+    pub file_name: Option<PathBuf>,
 }
 
-// This function takes a line and returns, respectively, the first and last digit, if they exist
-pub fn get_first_digit<I: Iterator<Item = char> + Clone>(
-    line_iter: I,
-    digit_map: &HashMap<&'static str, u32>,
-    trie: &Trie,
-) -> Option<u32> {
-    let mut result = None;
-    let mut searcher = trie.get_searcher();
-
-    let mut char_iter = line_iter.clone();
-    let mut last_word_iter = line_iter.clone();
-    while let Some(c) = char_iter.next() {
-        match searcher.advance(c) {
-            Ok(Some(val)) => {
-                for _ in 0..searcher.len() {
-                    last_word_iter.next().expect("should match char_iter");
-                }
-                let digit = digit_map
-                    .get(&val[..])
-                    .expect("must exist due to construction");
-                result = Some(*digit);
-                break;
-            }
-            Err(_) => {
-                // If we get no matches, we need to go all the way back to the first character we
-                // processed and skip just that one
-                last_word_iter
-                    .next()
-                    .expect("next should exist as we've seen a word before");
-                char_iter = last_word_iter.clone();
-                searcher = trie.get_searcher();
-            }
-            _ => {}
-        }
+pub fn read_lines(opt: &Opt) -> std::io::Result<Vec<String>> {
+    if let Some(file_name) = &opt.file_name {
+        read_lines_impl(File::open(file_name)?)
+    } else {
+        read_lines_impl(std::io::stdin())
     }
-
-    result
 }
 
-// Returns the first and last digit all the lines provided
-pub fn get_line_numbers<R: Read>(
-    r: R,
-    fwd_digit_map: &HashMap<&'static str, u32>,
-    bwd_digit_map: &HashMap<&'static str, u32>,
-) -> std::io::Result<Vec<u32>> {
-    let fwd_trie = get_trie(fwd_digit_map);
-    let bwd_trie = get_trie(bwd_digit_map);
-
+fn read_lines_impl<R: Read>(r: R) -> std::io::Result<Vec<String>> {
     let mut reader = BufReader::new(r);
-    let mut numbers = Vec::new();
-
+    let mut lines = Vec::new();
     loop {
         let mut line = String::new();
         if reader.read_line(&mut line)? == 0 {
             break;
         }
-
-        let first_digit = if let Some(d) = get_first_digit(line.chars(), &fwd_digit_map, &fwd_trie)
-        {
-            d
-        } else {
-            println!("line has no digits");
-            continue;
-        };
-        let last_digit =
-            if let Some(d) = get_first_digit(line.chars().rev(), &bwd_digit_map, &bwd_trie) {
-                d
-            } else {
-                println!("line has no digits");
-                continue;
-            };
-
-        numbers.push(first_digit * 10 + last_digit);
+        lines.push(line);
     }
-
-    Ok(numbers)
-}
-
-fn get_trie(digit_map: &HashMap<&'static str, u32>) -> Trie {
-    Trie::new(digit_map.iter().map(|(s, _)| *s))
+    Ok(lines)
 }
